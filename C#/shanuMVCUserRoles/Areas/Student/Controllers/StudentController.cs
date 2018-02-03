199d3ce17f5db1068a7f9c94670422719e19ad47
Nira <Nira@Vermiis>
2018-02-03 17:11:52 +0100

    modele dla studenta

--- C#/shanuMVCUserRoles/Areas/Student/Controllers/StudentController.cs
+++ C#/shanuMVCUserRoles/Areas/Student/Controllers/StudentController.cs
@@ -1,24 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using zarzadzanieTematami.Models;
 
 namespace shanuMVCUserRoles.Areas.Student.Controllers
 {
     public class StudentController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         // GET: Student/Student
         public ActionResult Index()
-        {
-            return View();
+        {          
+            return View(db.Topics.ToList().Where(x => x.IsTaken == false));
         }
 
         public ActionResult Create()
         {
-            TopicStudentViewModel model = new TopicStudentViewModel();
+            TopicCreateStudentViewModel model = new TopicCreateStudentViewModel();
+            model.IsProposed = true;
+            model.TakenBy = User.Identity.Name.ToString();
+            model.IsTaken = true;
 
             return View(model);
+        }
+
+        //REZERWACJA
+
+        public ActionResult Reservation(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Topics topics = db.Topics.Find(id);
+            if (topics == null)
+            {
+                return HttpNotFound();
+            }
+            return View(topics);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Reservation(TopicReservationStudentViewModel topics)
+        {
+            if (ModelState.IsValid)
+            {              
+                topics.TakenByID = User.Identity.Name.ToString();
+                topics.IsTaken = true;
+
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(topics);
         }
     }
 }
--- C#/shanuMVCUserRoles/Areas/Student/Models/ViewModels/TopicStudentViewModel.cs
+++ C#/shanuMVCUserRoles/Areas/Student/Models/ViewModels/TopicStudentViewModel.cs
@@ -1,7 +1,25 @@
-﻿public class TopicStudentViewModel
+﻿public class TopicReservationStudentViewModel
 {
-    public string Topic { get; set; }
+    public string TakenByID { get; set; }
+    public bool IsTaken { get; set; }
+}
+
+public class TopicCreateStudentViewModel
+{
+    public string Title { get; set; }
     public string Description { get; set; }
-    public string Promotor { get; set; }
+    public string PromotorName { get; set; }
+    public string TypeOf { get; set; }
+    public string TakenBy { get; set; }
+    public bool IsProposed { get; set; }
+    public bool IsTaken { get; set; }
+}
+
+public class TopicShowStudentViewModel
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string PromotorName { get; set; }
+    public string TypeOf { get; set; }
     public bool IsTaken { get; set; }
 }
