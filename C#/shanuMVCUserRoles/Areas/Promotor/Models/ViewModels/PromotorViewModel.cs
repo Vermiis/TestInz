80f04997070179f568cb00d5f4484937b813d5b8
Nira <Nira@Vermiis>
2018-02-06 20:40:06 +0100

    ViewModele dla promotora

--- C#/shanuMVCUserRoles/Areas/Promotor/Models/ViewModels/PromotorViewModel.cs
+++ C#/shanuMVCUserRoles/Areas/Promotor/Models/ViewModels/PromotorViewModel.cs
@@ -1,14 +1,28 @@
 ﻿public class PromotrAddTopicViewModel
 {
+    public string Title { get; set; }
+    public string Descripton { get; set; }
+    public string TypeOf { get; set; }
+    public string PromotorName { get; set; }
+    public string PromotorID { get; set; }
 
 }
 
 public class PromotorListTopicsViewModel
 {
+    public string Title { get; set; }
+    public string Descripton { get; set; }
+    public string TypeOf { get; set; }
+    public string TakenByID { get; set; }
 
 }
 
 public class PromotorEditTopicViewModel
 {
+    public string Title { get; set; }
+    public string Descripton { get; set; }
+    public string TypeOf { get; set; }
+    public bool IsAccepted { get; set; }
+    public bool IsProposed { get; set; }
 
 }
