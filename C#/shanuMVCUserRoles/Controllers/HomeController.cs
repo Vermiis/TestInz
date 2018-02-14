949bc6950b44f2a6db1dbb238f17cf2536ceb6cd
Nira <Nira@Vermiis>
2018-02-14 22:29:28 +0100

    poprawka z rolami

--- C#/shanuMVCUserRoles/Controllers/HomeController.cs
+++ C#/shanuMVCUserRoles/Controllers/HomeController.cs
@@ -6,26 +6,41 @@ using System.Web.Mvc;
 
 namespace zarzadzanieTematami.Controllers
 {
-	public class HomeController : Controller
-	{
-		public ActionResult Index()
-		{
-			return View();
-		}
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
 
-		public ActionResult About()
-		{
-			ViewBag.Message = "Your application description page.";
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
 
-			return View();
-		}
+            return View();
+        }
 
-		public ActionResult Contact()
-		{
-			ViewBag.Message = "Your contact page.";
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
 
-			return View();
-		}
+            return View();
+        }
+
+
+        /*
+        public ActionResult Index2()
+        {
+            if (User.IsInRole("Admin"))
+                return RedirectToAction("Index", "AdminController", new { area = "Admin" });
+            else if (User.IsInRole("Student"))
+                return RedirectToAction("Index", "UserController", new { area = "Student" });
+            else if (User.IsInRole("Promotor"))
+                return RedirectToAction("Index", "UserController", new { area = "Promotor" });
+                }*/
+
+
         
+
     }
 }
