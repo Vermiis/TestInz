7a450e4eaeffbe9ea4144752e933363de8548f98
Nira <Nira@Vermiis>
2018-02-22 20:32:46 +0100

    nowy viewmodel dla promotora + kontroler

--- C#/shanuMVCUserRoles/Areas/Promotor/Controllers/PromotorController.cs
+++ C#/shanuMVCUserRoles/Areas/Promotor/Controllers/PromotorController.cs
@@ -8,6 +8,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using zarzadzanieTematami.Models;
+using zarzadzanieTematami.Areas.Promotor.Models.ViewModels;
 
 namespace zarzadzanieTematami.Areas.Promotor.Controllers
 {
@@ -99,7 +100,38 @@ namespace zarzadzanieTematami.Areas.Promotor.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(PromotorEditTopicViewModel topics)
+        public ActionResult Edit(Topics topics)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(topics).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(topics);
+        }
+
+        // GET: Promotor/Promotor/Edit/5
+        public ActionResult AcceptProposition(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Topics topics = db.Topics.Find(id);
+            if (topics == null)
+            {
+                return HttpNotFound();
+            }
+            return View(topics);
+        }
+
+        // POST: Promotor/Promotor/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AcceptProposition(Topics topics)
         {
             if (ModelState.IsValid)
             {
---
+++ C#/shanuMVCUserRoles/Areas/Promotor/Models/ViewModels/TopicsAcceptViewModel.cs
@@ -0,0 +1,28 @@
+﻿using zarzadzanieTematami.Models;
+
+namespace zarzadzanieTematami.Areas.Promotor.Models.ViewModels
+{
+
+    public class TopicsAcceptViewModel
+    {
+        public TopicsAcceptViewModel() { }
+
+        public TopicsAcceptViewModel(Topics topic)
+        {
+            this.ID = topic.ID;
+            this.Title = topic.Title;
+            this.Details = topic.Details;
+            this.Commentary = topic.Commentary;
+            this.IsAccepted = topic.IsAccepted;
+            this.IsRejected = topic.IsRejected;
+
+        }
+
+        public int ID { get; private set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+        public string Commentary { get; set; }
+        public bool IsAccepted { get; set; }
+        public bool IsRejected { get; set; }
+    }
+}
