13a5f3f18684265eee8790cd6ef636a790159ae4
Nira <Nira@Vermiis>
2018-02-28 19:02:16 +0100

    zmiany w langach

--- C#/shanuMVCUserRoles/Models/AccountViewModels.cs
+++ C#/shanuMVCUserRoles/Models/AccountViewModels.cs
@@ -74,17 +74,17 @@ namespace zarzadzanieTematami.Models
 		public string Email { get; set; }
 
 		[Required]
-		[Display(Name = "UserName")]
+		[Display(Name = "Nazwa użytkownika")]
 		public string UserName { get; set; }
 
 		[Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "Hasło")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
+        [Display(Name = "Potwierdż hasło")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
